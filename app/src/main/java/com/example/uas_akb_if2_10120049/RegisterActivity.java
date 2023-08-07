f10c64d3f0a1341c89c1daca21956970fcafef22
Daniel Wattimury <salamanderttf@gmail.com>
2023-08-08 01:28:25 +0700

    Create Register Page and Setup Firebase

--- app/src/main/java/com/example/uas_akb_if2_10120049/RegisterActivity.java
+++ app/src/main/java/com/example/uas_akb_if2_10120049/RegisterActivity.java
@@ -4,26 +4,26 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
-import android.os.Handler;
 import android.view.View;
 import android.widget.TextView;
 
-public class LoginActivity extends AppCompatActivity {
+public class RegisterActivity extends AppCompatActivity {
 
     TextView textView;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_login);
+        setContentView(R.layout.activity_register);
 
         getSupportActionBar().hide();
 
-        textView = findViewById(R.id.register_link);
+        textView = findViewById(R.id.login_link);
 
         textView.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent intent = new Intent(LoginActivity.this, RegisterActivity.class);
+                Intent intent = new Intent(RegisterActivity.this, LoginActivity.class);
                 startActivity(intent);
             }
         });
